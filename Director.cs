e21dc722f3b9908bad9aa1fd91026c6ccd16d28d
a.amraei <amin.amraei@gmail.com>
2023-12-30 08:51:13 +0330

    Add project files.

---
+++ Director.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+        public void BuildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void BuildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+}
